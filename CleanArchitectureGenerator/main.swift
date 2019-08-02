ba938d412feff560d3cfdcdf7b570a0469f029cf
AndreyLebedev <lebedac@gmail.com>
2019-08-02 18:18:03 +0300

    xcode proj

---
+++ CleanArchitectureGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CleanArchitectureGenerator
+//
+//  Created by Yan Gerasimuk on 02/08/2019.
+//  Copyright © 2019 Clean Architecture Team. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
